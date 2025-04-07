e31321886ee765a90413970b7800ee1e0ffb0eda
Gowtham Vnr <gowthamvnr2468@gmail.com>
2025-04-07 20:38:02 +0530

    api requests

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from products import views
 
 urlpatterns  = [
-    path('hello/',views.hello)
+    path('hello/',views.hello),
+    path('allProducts/',views.get_products),
+    path('product/<int:id>',views.get_product)
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+
+from products.models import Products
+from rest_framework.decorators import api_view
+
+from products.serializers import ProductSerializer
+
 
 # Create your views here.
 def hello(request):
+    data = Products.objects.all()
+    print(data[0].name)
+    data[0].name = 'abc'
+    print(data[0].name)
     return HttpResponse('Hello World!, Have a good day')
+
+@api_view(['GET'])
+def get_products(request):
+    data = Products.objects.all()
+    serializedProducts = ProductSerializer(data, many=True)
+    return Response(serializedProducts.data)
+
+
+@api_view(['GET'])
+def get_product(request,id):
+    try:
+        data = Products.objects.get(id=id)
+        serializedProducts = ProductSerializer(data)
+        return Response(serializedProducts.data)
+    except Products.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
